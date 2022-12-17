c3638d8008c4152f5c6473a180bdacf4eef1e5aa
Nathaniel Nathaniel Asa <68902148+NattyGold@users.noreply.github.com>
2022-12-17 19:35:41 +0100

    Update main.js
    removed the prealoader action event listener from loading on the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,12 +116,12 @@
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
+//   let preloader = select('#preloader');
+//   if (preloader) {
+//     window.addEventListener('load', () => {
+//       preloader.remove()
+//     });
+//   }
 
   /**
    * Porfolio isotope and filter
